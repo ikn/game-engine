840a6774c79ee32b69c50b99f69fa6ae6ce1d208
Joseph Lansdowne <J49137@gmail.com>
2013-06-18 12:25:57 +0100

    EventHandler.add, .rm, __setitem__, __delitem__

--- game/engine/evt2.py
+++ game/engine/evt2.py
@@ -5,7 +5,7 @@ import pygame as pg
 # - how do domain filenames work?  Do we try loading from a homedir one first, then fall back to the distributed one?  Do we save to the homedir one?
 # - input recording and playback (allow whitelisting/excluding by registered event name)
 # - a way to alter loaded events/schemes, and all associated parameters
-# - a way to register new event types
+# - a way to register new input/event types (add to inputs_by_name/evts_by_name)
 # - can use part of an input, eg. for a button event, 'pad axis 0:0'; for an axis event, 'pos pad axis 2:1'
 #    - might not be >2-component inputs, but can do, eg. for an axis event, 'neg pos pad axis 0:0,1'
 # - error on names of events, schemes, domains clashing
@@ -19,31 +19,34 @@ evt_component_names = {
     4: ('left', 'right', 'up', 'down')
 }
 
+DOWN = 0
+UP = 1
+HELD = 2
+REPEAT = 3
+
 
 class Input (object):
     invalid_device_id = -1
     device_var = None
 
-    def __init__ (self):
-        if hasattr(self, 'pgevts'):
-            self.filters = {'type': set(self.pgevts)}
-        else:
-            self.filters = {}
+    def __init__ (self, *pgevts):
         self.evt = None
+        pgevts = set(pgevts)
+        if hasattr(self, 'pgevts'):
+            pgevts.update(self.pgevts)
+        self.filters = {}
+        if pgevts:
+            self.filters['type'] = pgevts
         self._device_assigned = self.device_var is None
-        self.pgevts = []
 
     def handle (self, pgevt):
         if not self._device_assigned:
             raise RuntimeError('an Input cannot be used if its device ID ' \
                                'corresponds to an unassigned variable')
-        self.pgevts.append(pgevt)
-        return True
-
-    def reset (self):
-        self.pgevts = []
+        return False
 
     def filter (self, attr, *vals, **kw):
+        # note cannot filter for None (have a module-wide UNFILTERABLE?)
         refilter = kw.get('refilter', False)
         if not vals:
             if refilter:
@@ -95,17 +98,37 @@ class Input (object):
             raise TypeError('this Input type doesn\'t support device IDs')
 
 
+class BasicInput (Input):
+    def __init__ (self, *pgevts):
+        Input.__init__(self, *pgevts)
+        self.pgevts = []
+
+    def handle (self, pgevt):
+        Input.handle(self, pgevt)
+        self.pgevts.append(pgevt)
+        return True
+
+    def reset (self):
+        self.pgevts = []
+
+
 class ButtonInput (Input):
     components = 1
 
     def __init__ (self):
+        self.btnevts = []
         self.held = False
 
     def down (self):
+        self.btnevts.append(DOWN)
         self.held = True
 
     def up (self):
+        self.btnevts.append(UP)
         self.held = False
+
+    def reset (self):
+        self.btnevts = []
 
 
 class KbdKey (ButtonInput):
@@ -152,25 +175,12 @@ class PadAxis (AxisInput):
 
 
 class Event (object):
-    input_types = Input
-    # event filtering by attributes - equal to, contained in
-    # sort filters by the amount they exclude
-    # note cannot filter for None
-    def __init__ (self):
+    input_types = BasicInput
+    def __init__ (self, *inputs):
         self.eh = None
         self.inputs = set()
+        self.add(*inputs)
         self.cbs = set()
-
-    def _set_eh (self, eh):
-        if self.eh is not None:
-            raise RuntimeError('an Event should not be added to more than ' \
-                               'one EventHandler')
-        self.eh = eh
-        eh._add_inputs(*self.inputs)
-
-    def _unset_eh (self):
-        self.eh._rm_inputs(*self.inputs)
-        self.eh = None
 
     def add (self, *inputs):
         types = self.input_types
@@ -178,7 +188,7 @@ class Event (object):
         eh_add = None if self.eh is None else self.eh._add_inputs
         for i in inputs:
             if not isinstance(i, types):
-                raise TypeError('{0} objects only accept inputs of type {1}' \
+                raise TypeError('{0} events only accept inputs of type {1}' \
                                 .format(type(self).__name__,
                                         tuple(t.__name__ for t in types)))
             self_add(i)
@@ -191,6 +201,7 @@ class Event (object):
 
     def cb (self, *cbs):
         self.cbs.update(cbs)
+        return self
 
     def respond (self):
         # maybe wrap with something else that handles the reset()
@@ -246,36 +257,94 @@ class Relaxis2 (MultiEvent):
     multiple = 2
 
 
-class EventHandler (object):
+class EventHandler (dict):
+    # is {name: Event|Scheme}
     def __init__ (self):
-        self.inputs = set()
+        self._unnamed = set()
+        self._inputs = set()
         self._filtered_inputs = ('type', {None: set()})
 
     def __contains__ (self, item):
         # can be event, scheme or name thereof
-        pass
-
-    def __getitem__ (self, item):
-        pass
+        return dict.__contains__(self, item) or item in self._unnamed
 
     def __setitem__ (self, item, val):
-        pass
+        self.add(**{item: val})
 
     def __delitem__ (self, item):
-        pass
+        self.rm(item)
 
     def add (self, *evts, **named_evts):
-        # kwargs contains domain=None
+        """add(*evts, **named_evts, domain = None)"""
         # can call with existing event to change domain; error if name exists
-        # events may be (pgevt, *cbs) to create an Event that just passes the pgevt to the cbs
+        # events may be (*(pgevts|cbs)) to create an Event that just passes the pgevt to the cbs
         # returns list of created events for positional args
-        # detect if domain kwarg was meant to be domain or event by its type
-        # on adding evt, call ._set_eh
-        pass
+        unnamed = self._unnamed
+        if 'domain' in named_evts:
+            domain = named_evts['domain']
+            if isinstance(domain, basestring):
+                del named_evts['domain']
+            else:
+                domain = None
+        else:
+            domain = None
+        # TODO: use domain
+        for evts in (((None, evt) for evt in evts), named_evts.iteritems()):
+            for name, evt in evts:
+                if not isinstance(evt, Event): # TODO: also Scheme
+                    # got (possibly mixed) list of pgevts/cbs
+                    pgevts = []
+                    cbs = []
+                    for item in evt:
+                        (cbs if callable(item) else pgevts).append(item)
+                    evt = Event(BasicInput(*pgevts)).cb(*cbs)
+                if evt.eh is not None:
+                    if evt.eh is self:
+                        # already own this event
+                        prev_name = evt._regname
+                        if name != prev_name:
+                            # change registered name
+                            # TODO: maybe need to let Scheme know about this
+                            if prev_name is None:
+                                unnamed.remove(evt)
+                            else:
+                                dict.__delitem__(self, prev_name)
+                            evt._regname = name
+                            if name is None:
+                                unnamed.add(evt)
+                            else:
+                                dict.__setitem__(self, name, evt)
+                    else:
+                        # owned by another handler
+                        raise RuntimeError('an Event should not be added to ' \
+                                           'more than one EventHandler')
+                else:
+                    # new event
+                    evt.eh = self
+                    evt._regname = name
+                    if name is None:
+                        unnamed.add(evt)
+                    else:
+                        dict.__setitem__(self, name, evt)
+                    self._add_inputs(*evt.inputs)
 
     def rm (self, *evts):
         # can be instances or names; if evt, call ._unset_eh
-        pass
+        unnamed = self._unnamed
+        # TODO: use domain
+        for evt in evts:
+            if isinstance(evt, basestring):
+                evt = self[evt] # raises KeyError
+            if evt.eh is self:
+                evt.eh = None
+                if evt._regname is None:
+                    unnamed.remove(evt)
+                else:
+                    dict.__delitem__(self, evt._regname)
+                evt._regname = None
+                self._rm_inputs(*evt.inputs)
+            else:
+                raise KeyError(evt)
 
     def _prefilter (self, filtered, filters, i):
         attr, filtered = filtered
@@ -306,7 +375,7 @@ class EventHandler (object):
                         prefilter(new_filtered, i.filters, i)
 
     def _add_inputs (self, *inputs):
-        add = self.inputs.add
+        add = self._inputs.add
         prefilter = self._prefilter
         filtered = self._filtered_inputs
         for i in inputs:
